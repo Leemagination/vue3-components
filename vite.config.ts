6a08138100100e0b2f3418e332496b57b8ed3dae
Leemagination <328445543@qq.com>
2024-03-25 22:31:44 +0800

    feat: 升级vue版本

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    port:8888
+  server: {
+    port: 8888
   },
-  plugins: [vue()],
+  plugins: [vue(), vueJsx()],
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment'
   }
-})
+});

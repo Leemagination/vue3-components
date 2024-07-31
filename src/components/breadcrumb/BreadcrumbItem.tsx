bdf2ac928b54e621ffc3b2b1c8406ccc15b34079
Leemagination <328445543@qq.com>
2024-07-31 22:42:35 +0800

    fix: 修复插槽渲染问题

--- src/components/breadcrumb/BreadcrumbItem.tsx
+++ src/components/breadcrumb/BreadcrumbItem.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, h, PropType } from 'vue';
+import { defineComponent, h, PropType, renderSlot } from 'vue';
 
 import './breadcrumbItem.scss';
 import { MouseFunc } from '../../config/interface';
@@ -22,9 +22,10 @@ const BreadcrumbItem = defineComponent({
   },
   render() {
     const { $slots } = this;
+    const slot = renderSlot($slots, 'default');
     return (
       <span class={['lee-breadcrumb-item']} onClick={(e) => this.handleCLick(e)}>
-        {this.href === null ? $slots : <a href={this.href}>{$slots}</a>}
+        {this.href === null ? slot : <a href={this.href}>{slot}</a>}
       </span>
     );
   }

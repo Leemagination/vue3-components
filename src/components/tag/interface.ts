7489effe8c633c132499d7d29e0358813e47c735
Leemagination <328445543@qq.com>
2024-12-04 23:01:49 +0800

    feat: 修改标签组件样式

--- src/components/tag/interface.ts
+++ src/components/tag/interface.ts
@@ -1,6 +1,8 @@
+import { CSSProperties } from '@vue/runtime-dom';
+
 export const tagType = ['default', 'primary', 'success', 'danger', 'warning'];
 export type TagType = 'default' | 'primary' | 'success' | 'danger' | 'warning';
-export interface CustomColor {
+export interface CustomColor extends CSSProperties {
   backgroundColor: string;
   borderColor: string;
   color: string;

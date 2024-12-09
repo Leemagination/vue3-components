b17e469ad83fb2c39409880b687276d84610be46
Leemagination <328445543@qq.com>
2024-12-10 00:03:57 +0800

    feat: 时间选择器增加清空

--- src/components/timePicker/TimePicker.tsx
+++ src/components/timePicker/TimePicker.tsx
@@ -51,6 +51,25 @@ function formatTimeText(val: number) {
   return `0${val}`;
 }
 
+const clearIcon = (
+  <span class="lee-time-picker-clear-icon">
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 12 12"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g fill="currentColor" fill-rule="nonzero">
+          <path d="M2.08859116,2.2156945 L2.14644661,2.14644661 C2.32001296,1.97288026 2.58943736,1.95359511 2.7843055,2.08859116 L2.85355339,2.14644661 L6,5.293 L9.14644661,2.14644661 C9.34170876,1.95118446 9.65829124,1.95118446 9.85355339,2.14644661 C10.0488155,2.34170876 10.0488155,2.65829124 9.85355339,2.85355339 L6.707,6 L9.85355339,9.14644661 C10.0271197,9.32001296 10.0464049,9.58943736 9.91140884,9.7843055 L9.85355339,9.85355339 C9.67998704,10.0271197 9.41056264,10.0464049 9.2156945,9.91140884 L9.14644661,9.85355339 L6,6.707 L2.85355339,9.85355339 C2.65829124,10.0488155 2.34170876,10.0488155 2.14644661,9.85355339 C1.95118446,9.65829124 1.95118446,9.34170876 2.14644661,9.14644661 L5.293,6 L2.14644661,2.85355339 C1.97288026,2.67998704 1.95359511,2.41056264 2.08859116,2.2156945 L2.14644661,2.14644661 L2.08859116,2.2156945 Z"></path>
+        </g>
+      </g>
+    </svg>
+  </span>
+);
+
 const setup = (props: ExtractPropTypes<typeof timePickerProps>) => {
   const timeValue = useModel(props, 'value');
   const timeText = computed(() => {
@@ -79,6 +98,24 @@ const setup = (props: ExtractPropTypes<typeof timePickerProps>) => {
   const hourListRef = ref<InstanceType<typeof NumList> | null>(null);
   const minuteListRef = ref<InstanceType<typeof NumList> | null>(null);
   const secondListRef = ref<InstanceType<typeof NumList> | null>(null);
+
+  const mouseHover = ref(false);
+  const showClearIcon = computed(() => {
+    if (mouseHover.value && props.clearable && timeText.value) {
+      return true;
+    }
+    return false;
+  });
+
+  function changeHoverStatus(status: boolean) {
+    mouseHover.value = status;
+  }
+
+  function clearTimeValue(e: MouseEvent) {
+    e.stopPropagation();
+    e.preventDefault();
+    timeValue.value = null;
+  }
 
   watch(timeText, async () => {
     await nextTick();
@@ -170,6 +207,9 @@ const setup = (props: ExtractPropTypes<typeof timePickerProps>) => {
     hourListRef,
     minuteListRef,
     secondListRef,
+    showClearIcon,
+    changeHoverStatus,
+    clearTimeValue,
     setHour,
     setMinute,
     setSecond,
@@ -189,10 +229,12 @@ const TimePicker = defineComponent({
           ref="targetRef"
           class={['lee-time-picker-container', this.disabled ? 'lee-time-picker-disabled' : null]}
           tabindex="0"
+          onMouseenter={() => this.changeHoverStatus(true)}
+          onMouseleave={() => this.changeHoverStatus(false)}
           onClick={() => this.handlePickerVisible()}
         >
           <span class="lee-time-picker-value">{this.timeText}</span>
-          {timeIcon}
+          {this.showClearIcon ? <clearIcon onClick={this.clearTimeValue}></clearIcon> : timeIcon}
           {this.timeText ? null : <div class="lee-time-picker-placeholder">{this.placeholder}</div>}
         </div>
         <Teleport to="body">
